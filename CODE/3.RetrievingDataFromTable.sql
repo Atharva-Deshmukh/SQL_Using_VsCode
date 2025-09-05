87c97f22e76ef0d956c9fbc3f5ce213fc20c18d8
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-09-05 17:44:52 +0530

    Retrieving data from table

--- CODE/3.RetrievingDataFromTable.sql
+++ CODE/3.RetrievingDataFromTable.sql
@@ -13,3 +13,48 @@ SELECT FirstName, Address FROM Persons;
 use myDB;
 SELECT PersonID + 100 FROM Persons;
 -- This will add 100 to all values in PersonID column and show the result
+
+-- -------------------------SELECT Vs DESCRIBE--------------------------------------------  
+
+                                    DESCRIBE student;
+
+Shows the structure (schema) of the table.
+
+Its not a DDL, DQL, DCL, TCL or a DML.
+
+It’s a metadata query command (in MySQL and some other DBs), often 
+considered part of DQL, but not officially classified in standard SQL.
+
+Output includes:
+- Column names
+- Data types
+- Whether NULL is allowed
+- Keys (PRI, UNI, MUL)
+- Default values
+- Extra info (like AUTO_INCREMENT)
+
+        Example output:
+
+        +----------+-------------+------+-----+---------+----------------+
+        | Field    | Type        | Null | Key | Default | Extra          |
+        +----------+-------------+------+-----+---------+----------------+
+        | id       | int(11)     | NO   | PRI | NULL    | auto_increment |
+        | name     | varchar(50) | YES  |     | NULL    |                |
+        | age      | int(11)     | YES  |     | NULL    |                |
+        +----------+-------------+------+-----+---------+----------------+
+
+
+👉 Schema metadata, not actual data.
+
+SELECT * FROM student;
+
+Returns actual row data from the table.
+* means “all columns”.
+Output will be the stored records:
+
++----+-------+-----+
+| id | name  | age |
++----+-------+-----+
+| 1  | Alice | 21  |
+| 2  | Bob   | 22  |
++----+-------+-----+
