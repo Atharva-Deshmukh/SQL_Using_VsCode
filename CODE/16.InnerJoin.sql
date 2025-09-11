644cc0b45b553a588eadff9b757215c4b77614ba
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-09-11 16:53:56 +0530

    Inner join

--- CODE/16.InnerJoin.sql
+++ CODE/16.InnerJoin.sql
@@ -1,10 +1,40 @@
 /*  What is an SQL INNER JOIN?
-Imagine you have two tables of data. An INNER JOIN is like connecting these two tables based on a 
+An INNER JOIN is like connecting these two tables based on a 
 matching piece of information (a common key) and only keeping the rows that have a match in both tables.
 
-Simple Analogy: You have a list of Students and a list of Courses. An Enrollment table connects them. 
-An INNER JOIN between Students and Enrollment would give you only the students who are enrolled in a 
-course and the details of their enrollment. Students who aren't enrolled in anything are left out.
+Only the records with common keys are extracted
+
+t1
++--------+---------+
+| UserID | UserName|
++--------+---------+
+|   1    | John    |
+|   2    | Emma    |
+|   3    | Ravi    |
+|   4    | Raj     |
++--------+---------+
+
+
+t2
++--------+----------+----------+
+| UserID |   Sub    | UserName |
++--------+----------+----------+
+|   3    | Maths    | Jarret   |
+|   4    | English  | Erina    |
+|   5    | Science  | Prashant |
+|   6    | SSC      | Rajan    |
++--------+----------+----------+
+
+SELECT t1.UserID, t1.UserName, t2.Sub
+FROM t1
+INNER JOIN t2 ON t1.UserID = t2.UserID;
+
+# UserID	UserName	Sub
+----------------------------
+     3	      Ravi	   Maths         --> Common data is extracted
+     4	      Raj	 English
+
+
 
 Some Real life applications:
 -----------------------------
@@ -45,6 +75,9 @@ UserName	Title
 John	   SQL Basics
 Emma	   Advanced Python
 John	   Blogging Tips
+
+"In real life, we use separate tables because of the normalization concept, which helps avoid data redundancy. 
+Joins allow us to combine data from multiple tables to get meaningful results.
 
 
 It is a good practice to include the table name when specifying columns in the SQL statement.
