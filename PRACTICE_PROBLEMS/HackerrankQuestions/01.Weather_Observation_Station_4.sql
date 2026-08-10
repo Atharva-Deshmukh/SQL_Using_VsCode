08822882b841441fd6166a21c3a1ae5a95da9237
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-08-10 19:53:23 +0530

    weather observation station 4

--- PRACTICE_PROBLEMS/HackerrankQuestions/01.Weather_Observation_Station_4.sql
+++ PRACTICE_PROBLEMS/HackerrankQuestions/01.Weather_Observation_Station_4.sql
@@ -3,3 +3,25 @@ Find the difference between the total number of CITY entries in the table
 and the number of distinct CITY entries in the table.
 */
 
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION;
+
+/*
+
+The important thing is that SELECT isn't actually being applied separately to both.
+
+SQL first looks at --> FROM STATION
+So it gets the rows from STATION.
+
+Then it evaluates the expression after SELECT --> COUNT(CITY) - COUNT(DISTINCT CITY)
+
+    You can think of it roughly like:
+
+    COUNT(CITY)              → 100
+    COUNT(DISTINCT CITY)     →  80
+                              ----
+                                20
+
+The SELECT simply says --> "Return the result of this expression."
+So: SELECT 100 - 80 would return: 20
+
+*/
