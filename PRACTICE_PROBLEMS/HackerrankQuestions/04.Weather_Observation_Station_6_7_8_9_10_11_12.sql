e2d703241baa314608060580eeb148767d4b81eb
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-08-10 21:29:40 +0530

    12th added

--- PRACTICE_PROBLEMS/HackerrankQuestions/04.Weather_Observation_Station_6_7_8_9_10_11_12.sql
+++ PRACTICE_PROBLEMS/HackerrankQuestions/04.Weather_Observation_Station_6_7_8_9_10_11_12.sql
@@ -44,3 +44,13 @@ end with vowels. Your result cannot contain duplicates.
 SELECT DISTINCT CITY FROM STATION
 WHERE CITY REGEXP '^[^a,e,i,o,u].*$'
    OR CITY REGEXP '^.*[^a,e,i,o,u]$';
+
+/*
+Query the list of CITY names from STATION that do not start with vowels and do 
+not end with vowels. Your result cannot contain duplicates.
+*/
+
+SELECT DISTINCT CITY FROM STATION 
+WHERE CITY REGEXP '^[^a,e,i,o,u].*$'
+AND
+CITY REGEXP '^.*[^a,e,i,o,u]$';
