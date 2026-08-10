10d00d643f849f756c4cd2e3073d55ed0feb0ba2
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-08-10 21:19:56 +0530

    10th added

--- PRACTICE_PROBLEMS/HackerrankQuestions/04.Weather_Observation_Station_6_7_8_9_10.sql
+++ PRACTICE_PROBLEMS/HackerrankQuestions/04.Weather_Observation_Station_6_7_8_9_10.sql
@@ -28,3 +28,10 @@ Your result cannot contain duplicates.
 */
 
 SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '^[^a,e,i,o,u].*$'
+
+/*
+Query the list of CITY names from STATION that do not end with vowels. 
+Your result cannot contain duplicates.
+*/
+
+SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '^.*[^a,e,i,o,u]$'; 
