3f56efd0a2369c2ea158391566b574f1102d9af4
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-08-10 21:04:18 +0530

    HigherThan75Marks

---
+++ PRACTICE_PROBLEMS/HackerrankQuestions/03.HigherThan75Marks.sql
@@ -0,0 +1,26 @@
+/*
+
+Query the Name of any student in STUDENTS who scored higher than  Marks. 
+Order your output by the last three characters of each name. 
+If two or more students both have names ending in the same last three characters 
+(i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+
+
+Important functions to be remebered:
+-------------------------------------
+
+First 2 characters	--> LEFT(Name, 2)
+First 3 characters	--> LEFT(Name, 3)
+Last 2 characters	--> RIGHT(Name, 2)
+Last 3 characters	--> RIGHT(Name, 3)
+
+SUBSTRING(string, starting_position, number_of_characters)
+
+Take 3 chars from postition 2	        -->     SUBSTRING(Name, 2, 3)
+Take 3 chars from 3rd position from end -->     SUBSTRING(Name, -3, 3)
+*/
+
+SELECT Name FROM STUDENTS 
+WHERE Marks > 75
+ORDER BY SUBSTRING(Name, -3, 3) ASC, ID ASC; 
+
