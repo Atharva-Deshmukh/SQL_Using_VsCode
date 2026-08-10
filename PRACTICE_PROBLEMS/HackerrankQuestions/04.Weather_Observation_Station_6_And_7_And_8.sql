b0b5b636ce26dd601f67795811b8549313ec37eb
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-08-10 21:14:49 +0530

    6 7 and 8

--- PRACTICE_PROBLEMS/HackerrankQuestions/04.Weather_Observation_Station_6_And_7_And_8.sql
+++ PRACTICE_PROBLEMS/HackerrankQuestions/04.Weather_Observation_Station_6_And_7_And_8.sql
@@ -12,3 +12,11 @@ Your result cannot contain duplicates.
 */
 
 SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '^.*[a,e,i,o,u]$';
+
+
+/*
+Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) 
+as both their first and last characters. Your result cannot contain duplicates.
+*/
+
+SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '^[a,e,i,o,u].*[a,e,i,o,u]$';
