b60166458a7cd08fe63fd9517bde8c46cf1fbfbe
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-08-14 20:37:10 +0530

    Joins theory

---
+++ SQL_CONCEPTS_WITH_CODE/06.SQL_Joins/01.Joins_Theory.sql
@@ -0,0 +1,21 @@
+/*
+===========================================================
+SQL JOINS → Combines rows from 2+ tables using a related column.
+===========================================================
+
+===========================================================
+QUICK VISUAL
+===========================================================
+
+        LEFT TABLE        RIGHT TABLE
+
+INNER   [ MATCHING ONLY ]
+
+LEFT    [ ALL LEFT + MATCHING RIGHT ]
+
+RIGHT   [ MATCHING LEFT + ALL RIGHT ]
+
+FULL    [ ALL LEFT + ALL RIGHT ]
+
+CROSS   [ EVERY LEFT × EVERY RIGHT ]
+*/
