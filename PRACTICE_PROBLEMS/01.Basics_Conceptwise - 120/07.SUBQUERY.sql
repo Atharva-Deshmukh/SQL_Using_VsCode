6086807c6bf8da86ba56acf0eddd747ee73fa265
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-08-22 13:06:04 +0530

    Subquery

--- PRACTICE_PROBLEMS/01.Basics_Conceptwise - 120/07.SUBQUERY.sql
+++ PRACTICE_PROBLEMS/01.Basics_Conceptwise - 120/07.SUBQUERY.sql
@@ -50,16 +50,102 @@ PROBLEMS
 
 1. Find employees earning above the overall average salary.
 
+   Thought process: select only the rows with above average salary
+                    avg salary to be calculated via subquery
+
+   SELECT name, salary 
+   FROM Employee
+   WHERE salary > (SELECT AVG(salary) FROM Employee);
+
 2. Find employee(s) with the maximum salary.
+
+   Thought process: select only the rows with max salary
+                    max salary to be calculated via subquery
+
+   SELECT name, salary 
+   FROM Employee
+   WHERE salary = (SELECT MAX(salary) FROM Employee);
 
 3. Find employees earning above the minimum salary.
 
-4. Find employees working in departments located in Bangalore.
+   Thought process: select only the rows with > min salary
+                    min salary to be calculated via subquery
+
+      SELECT name, salary
+      FROM Employee
+      WHERE salary > (SELECT MIN(salary) FROM Employee);
+
+4. Find employees working in departments located in Bangalore.    --> REVISE
+
+   Thought process: Way-1: Using joins
+                    Join the tables to get name, department_name, location_name using INNER joins to 
+                    avoid null
+                    where city =  Bangalore
+
+                    Way-2: Using subquery
+                           Work backwards: Location -> Department -> Employee
+
+                           Return location_ids with city = 'Bangalore'
+                           Return department_ids with the above location_ids
+                           Return names with the above department_ids
+
+         SELECT name 
+         FROM Employee 
+         WHERE department_id IN (        -- Filter first tables department_ids based on inner tables values
+            SELECT department_id 
+            FROM Department 
+            WHERE location_id IN (     -- Filter those department_ids based on location_ids returned from inner query
+               SELECT location_id
+               FROM Location 
+               WHERE city = 'Bangalore'
+            )
+         );
 
 5. Find employees whose department has more than 1 employee.
 
-6. Find employees earning above their own department average.
+   Thought process: Find names based on department_ids from subquery
+                    subquery returns department_ids with count > 1
+
+      SELECT name 
+      FROM Employee
+      WHERE department_id IN (
+         SELECT department_id
+         FROM Employee 
+         GROUP BY department_id
+         HAVING COUNT(*) > 1
+      );
+
+6. Find employees earning above their own department average.         -->   REVISE
+
+   Thought process: filter employees in outer query based on inner query
+                    inner query returns avg salary of the current employee department only
+
+                    Since we need to determine which department the current employee belongs to
+
+                    This creates dependency of inner query on the outer query, hence we used
+                    correlated subquery
+
+      SELECT name, salary
+      FROM Employee AS outer_e
+      WHERE salary > (
+         SELECT AVG(salary) FROM Employee AS inner_e
+         WHERE inner_e.department_id = outer_e.department_id   -- CALCULATE THE avg salary of current employee's dept only
+      );
 
 7. Find employees having the highest salary in their department.
 
-8. Find employees whose salary is greater than their department average.
+   Thought process: filter employees in outer query based on inner query
+                    inner query returns hightest salary of the current employee department only
+
+                    This creates dependency of inner query on the outer query, hence we used
+                    correlated subquery
+
+      SELECT name, salary
+      FROM Employee AS outer_e
+      WHERE salary = (
+         SELECT MAX(salary) 
+         FROM Employee AS inner_e
+         WHERE inner_e.department_id = outer_e.department_id
+      );
+
+8. Find employees whose salary is greater than their department average. -- Done already!
