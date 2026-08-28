420a44044b6efedf2cd22b8228573eb7ef0ef530
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-08-28 19:54:31 +0530

    Joins problems Leetcode

---
+++ PRACTICE_PROBLEMS/03.Leetcode - 105/01.Joins/07.IMP_1965_MissingInformation.sql
@@ -0,0 +1,89 @@
+/* 1965. Employees With Missing Information
+
+Table: Employees
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
++-------------+---------+
+employee_id is the column with unique values for this table.
+Each row of this table indicates the name of the employee whose ID is employee_id.
+ 
+
+Table: Salaries
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| salary      | int     |
++-------------+---------+
+employee_id is the column with unique values for this table.
+Each row of this table indicates the salary of the employee whose ID is employee_id.
+ 
+
+Write a solution to report the IDs of all the employees with missing information. 
+The information of an employee is missing if:
+
+The employee's name is missing, or
+The employee's salary is missing.
+Return the result table ordered by employee_id in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employees table:
++-------------+----------+
+| employee_id | name     |
++-------------+----------+
+| 2           | Crew     |
+| 4           | Haven    |
+| 5           | Kristian |
++-------------+----------+
+Salaries table:
++-------------+--------+
+| employee_id | salary |
++-------------+--------+
+| 5           | 76071  |
+| 1           | 22517  |
+| 4           | 63539  |
++-------------+--------+
+Output: 
++-------------+
+| employee_id |
++-------------+
+| 1           |
+| 2           |
++-------------+
+Explanation: 
+Employees 1, 2, 4, and 5 are working at this company.
+The name of employee 1 is missing.
+The salary of employee 2 is missing.
+
+
+*/
+
+-- Here, we need unique parts in both tables, excluding the common part
+-- Full join is not supported in MySQL, hence UNION solution is needed
+-- NOTE: For UNION, the selected columns must have matching data types
+SELECT Employees.employee_id AS employee_id
+FROM Employees
+WHERE Employees.employee_id NOT IN (
+    SELECT employee_id FROM Salaries
+)
+
+UNION
+
+SELECT Salaries.employee_id AS employee_id
+FROM Salaries
+WHERE Salaries.employee_id NOT IN (
+    SELECT employee_id FROM Employees
+)
+
+ORDER BY employee_id;
